2ec6bb7d6160bf8749d00ff769fc2c27d0a25200
Cristobal Jara <jara@admins-iMac-2.local>
2017-10-13 16:25:29 -0700

    Add function reverse

--- lab0ct13.js
+++ lab0ct13.js
@@ -11,3 +11,17 @@ var abc  = rotateLeft3(numbers);
 console.log(rotateLeft3([1, 2, 3]));
 console.log(rotateLeft3([5, 11, 9]));
 console.log(rotateLeft3([7, 0, 0]));  
+
+
+function reverse3(word){
+  return word.reverse();
+
+}
+
+var numbers = [];
+var abc  = reverse3(numbers);
+
+console.log(reverse3([1, 2, 3]));
+console.log(reverse3([5, 11, 9]));
+console.log(reverse3([7, 0, 0]));  
+
