b8afc2d22eabd444f749219cd8058178713e3b90
Cristobal Jara <jara@admins-iMac-2.local>
2017-10-13 16:42:15 -0700

    Add function has2 and 3

--- lab0ct13.js
+++ lab0ct13.js
@@ -25,3 +25,12 @@ console.log(reverse3([1, 2, 3]));
 console.log(reverse3([5, 11, 9]));
 console.log(reverse3([7, 0, 0]));  
 
+function has23(a){
+ return (a [0]==2|| a [1]==3)||(a [1]==2|| a [0]==3);
+}
+
+var numbers = [];
+
+console.log(has23([2, 5]));
+console.log(has23([4, 3]));
+console.log(has23([4, 5]));  
